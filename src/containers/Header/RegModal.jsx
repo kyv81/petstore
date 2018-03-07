2773df1a9fa5e081dfacca15e9fda8281676d1ef
Stanislav Ezra Babushkin <playmarketstas@gmail.com>
2018-03-08 01:52:50 +0300

    добавлен хэдер с запуском и переключением модалок

---
+++ src/containers/Header/RegModal.jsx
@@ -0,0 +1,67 @@
+import React, { Fragment } from 'react';
+import { string } from 'prop-types';
+// import styles from './index.css';
+import { connect } from 'react-redux';
+import { tryLogin } from 'actions/user';
+import { func } from 'prop-types';
+
+// подключил компонент к стору чтобы иметь тут dispatch
+// @connect()
+export default class AuthModal extends React.PureComponent {
+  // у модалки есть свой state, который потом отдается в на проверку в БД
+  // делаем свой state, чтобы перерендеривались только компоннеты модолки, а не все приложение
+  state = {
+    email: '',
+    password: '',
+  };
+
+  static propTypes = {
+    dispatch: func,
+  };
+
+  onChangeEmail = e => {
+    this.setState({ email: e.target.value });
+  };
+  onChangePassword = e => {
+    this.setState({ password: e.target.value });
+  };
+
+  onAuth = e => {
+    const { dispatch } = this.props;
+    const { email, password } = this.state;
+    e.preventDefault();
+    // делаем диспатч в стор, чтобы он сазал нам есть ли такой юзер
+    dispatch(tryLogin(email, password))
+      .then(() => {
+        console.log('успешно');
+      })
+      .catch(() => {
+        console.log('провал(((');
+      });
+  };
+
+  render() {
+    return (
+      <div >
+        <h4>Авторизация</h4>
+        <form onSubmit={this.onAuth} id="authFormID">
+          <label>
+            <div>Емэйл</div>
+            <input onChange={this.onChangeEmail} type="text" name="email" />
+          </label>
+          <label>
+            <div>Пароль</div>
+            <input
+              onChange={this.onChangePassword}
+              type="text"
+              name="password"
+            />
+          </label>
+          <button className="btn waves-effect waves-light" type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
--- src/containers/Header/index.jsx
+++ src/containers/Header/index.jsx
@@ -1,16 +1,69 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import styles from './index.css';
+import RegModal from '/containers/Header/RegModal';
+import Button from 'Components/Button';
+
+const display = {
+  display: 'block'
+};
+const hide = {
+  display: 'none'
+};
 
 export class Header extends React.PureComponent {
+
+  constructor(props) {
+    super(props);
+    this.toggleAuth = this.toggleAuth.bind(this);
+    this.toggleReg= this.toggleReg.bind(this);
+
+    this.state = {
+      toggle: '',
+    }
+  };
+
+
+  toggleAuth(event) {
+    this.setState(toggle => ({
+      toggle: 'auth'
+    }));
+  }
+
+  toggleReg(event) {
+    this.setState(toggle => ({
+      toggle: 'reg'
+    }));
+  }
+
   render() {
-    return (
-      <div className="container">
-        <Link className="btn" to="/" href="/">Главная</Link>
-        <Link className="btn" to="/shop" href="/shop">Магазин</Link>
-        <Link className="btn" to="/cabinet" href="/cabinet">Личный кабинет</Link>
+    const { user: { isLoggedIn } } = this.props.store.getState();
+    var modal = [];
+    modal.push(
+      <div className="modal" style={this.state.toggle ? display : hide}>
+        <div className="modal-content">
+                {this.state.toggle=='reg'?<RegModal/>:'nope'}
+        </div>
       </div>
+    );
+    return (
+      <Fragment>
+        <Link to='/' href='/' className='btn'>
+          PetShop
+        </Link>
+        <Link className='btn' to='/shop' href='/shop'>
+          Магазин
+        </Link>
+        {!isLoggedIn ? (
+          <div>
+            <Button className="btn" onClick={this.toggleAuth}>Авторизация</Button>
+            <Button className="btn" onClick={this.toggleReg}>Регистрация</Button>
+            {modal}
+          </div>
+        ) : (null)
+        }
+      </Fragment>
     );
   }
 }
-
 export default Header;
